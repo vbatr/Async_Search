65a7415c3b805e58e4331cc0a4034a0c1a9915fc
vbatr <volodymyr.batrukh@gmail.com>
2016-07-01 18:22:46 +0300

    Searches results on a given webpage

---
+++ Async_Search/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebPageProcessor webPageProcessor = new WebPageProcessor();
+            webPageProcessor.FindHtmlPageElementsAsync("http://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp", ".carousel-inner");
+
+            //simulating long running work in Main. Interaction with UI for example
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main(): processing...");
+                //add some dalay
+                Thread.Sleep(200);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Async_Search/WebPageProcessor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Async_Search
+{
+    public class WebPageProcessor
+    {
+        /// <summary>
+        /// Searches and displays formatted result for given search pattern
+        /// </summary>
+        /// <param name="webPageUri">Requested web page uri</param>
+        /// <param name="searchPattern">Search pattern</param>
+        /// <returns></returns>
+        public async Task FindHtmlPageElementsAsync(string webPageUri, string searchPattern)
+        {
+            if (string.IsNullOrEmpty(webPageUri))
+                throw new ArgumentNullException("webPageUri");
+            if (string.IsNullOrEmpty(webPageUri))
+                throw new ArgumentNullException("searchPattern");
+
+            try
+            {
+                //get html page asynchronously
+                var htmlString = await GetHtmlPageAsync(webPageUri);
+
+                if (string.IsNullOrEmpty(htmlString))
+                    return;
+
+                //Find html elements and show formatted result
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(htmlString);
+
+                var foundRowsList =
+                    htmlDocument.DocumentNode.SelectNodes("//tr")
+                        .Where(n => n.InnerHtml.Contains(searchPattern))
+                        .ToList();
+
+                if (!foundRowsList.Any())
+                {
+                    Console.WriteLine("No results found for given search pattern: {0}", searchPattern);
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("***Search results***");
+                    foreach (var row in foundRowsList)
+                    {
+                        var formatedResult = new StringBuilder();
+
+                        foreach (var child in row.ChildNodes)
+                        {
+                            formatedResult.Append(child.InnerText);
+                            formatedResult.Append("\t");
+                        }
+                        Console.WriteLine(formatedResult.ToString());
+                    }
+                    Console.WriteLine(Environment.NewLine);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Processing of search results were stopped");
+            }
+        }
+
+        /// <summary>
+        /// Gets html page asynchronously
+        /// </summary>
+        /// <param name="webPageUri">Requested web page uri</param>
+        /// <returns></returns>
+        public async Task<string> GetHtmlPageAsync(string webPageUri)
+        {
+            if (string.IsNullOrEmpty(webPageUri))
+                throw new ArgumentNullException("webPageUri");
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    Task<string> httpClientTask = client.GetStringAsync(webPageUri);
+
+                    //We can do additional processing here
+
+                    string result = await httpClientTask;
+
+                    return result;
+                }
+                catch (HttpRequestException)
+                {
+                    Console.WriteLine("Requested page not found");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
